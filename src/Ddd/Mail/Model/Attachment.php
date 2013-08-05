f522e747e944802b47bddf85f9aaee89a8813283
slavcopost <slavcopost@gmail.com>
2013-08-05 13:47:01 +0300

    Add mime-type property to Attachment

--- src/Ddd/Mail/Model/Attachment.php
+++ src/Ddd/Mail/Model/Attachment.php
@@ -5,15 +5,22 @@ namespace Ddd\Mail\Model;
 class Attachment
 {
     private $path;
+    private $mimeType;
 
-    public function __construct($path)
+    public function __construct($path, $mimeType)
     {
         $this->path = $path;
+        $this->mimeType = $mimeType;
     }
 
     public function getPath()
     {
         return $this->path;
+    }
+
+    public function getMimeType()
+    {
+        return $this->mimeType;
     }
 
     public function __toString()
